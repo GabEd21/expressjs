0f6791dc632aeb04710da6105c71434b65f2ead9
GabEd21 <cegdumandan@gmail.com>
2024-04-15 20:02:32 +0800

    added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 
 var weather = require('weather-js');
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+const about = require('./module');
+
 
  
 // Set EJS as the view engine
@@ -30,17 +34,64 @@ app.get('/davao', (req, res) => {
     });
 });
 
+app.get('/canada', (req, res) => {
+    weather.find({search: 'Canada', degreeType: 'F'}, function(err, result) {
+        if(err) console.log(err);
+        else{
+            let data = {
+                weathercanada: eval(JSON.stringify(result, null, 2))
+            }
+            res.render('canada', data)
+        }
+    });
+});
+
+app.get('/japan', (req, res) => {
+    weather.find({search: 'Japan', degreeType: 'F'}, function(err, result) {
+        if(err) console.log(err);
+        else{
+            let data = {
+                weatherjapan: eval(JSON.stringify(result, null, 2))
+            }
+            res.render('japan', data)
+        }
+    });
+});
+
+app.get('/thailand', (req, res) => {
+    weather.find({search: 'Thailand', degreeType: 'F'}, function(err, result) {
+        if(err) console.log(err);
+        else{
+            let data = {
+                weatherthailand: eval(JSON.stringify(result, null, 2))
+            }
+            res.render('thailand', data)
+        }
+    });
+});
+
+app.get('/singapore', (req, res) => {
+    weather.find({search: 'Singapore', degreeType: 'F'}, function(err, result) {
+        if(err) console.log(err);
+        else{
+            let data = {
+                weathersingapore: eval(JSON.stringify(result, null, 2))
+            }
+            res.render('singapore', data)
+        }
+    });
+});
+
 app.get('/index', (req, res) => {
     res.render('index', { username: 'John', para: 'paragraph' });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { name: 'Christian Ed Gabriel S. Dumandan', title: 'My Life', paragraph: 'Hello! I am Christian Ed Gabriel S. Dumandan, when I was high school I was known as a drug lord and some of the teachers called me that, However since it was a famous word the decided to take me on a Drug test In the letter it says I was randomly and I recceive this letter twice in a month I am also a very known as Padre Damaso ',
-                sech: 'The Unexpected Role', parasec: 'INT. HIGHSCHOOL CLASSROOM - Afternoon', paragraph2: '', bold1: 'Female Teacher:', bold2: 'MULTIPLE STUDENT:' });
+    res.render('about', { about });
 });
 
 app.get('/portfolio', (req, res) => {
-    res.render('index', { username: 'John', para: 'paragraph' });
+    res.render('portfolio', { name: 'Christian Dumandan', para: 'paragraph' });
 });
 
 app.use((req, res) => {
